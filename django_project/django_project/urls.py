00ad115eded20d4559f047abc78371ad4ab84e5b
Kundan547 <kundanvyas197@gmail.com>
2025-01-22 11:03:00 +0530

    Added pasword reset, password done, password confirm and also password complete and google authentication are completed

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -10,7 +10,11 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),  # User registration page
     path('profile/', user_views.profile, name='profile'),  # User profile page
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),  # Login page
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),  # Logout page
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),# Logout page
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'), #Password-reset page
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'), #Password-reset done page.
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'), #Password-reset confirm page.
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), name='password_reset_complete'), #Password-reset-complete page.
     path('', include('blog.urls')),  # Include blog app's URL patterns
 ]
 
